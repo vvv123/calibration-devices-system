3b3b05c580cdf517345acf88a5032d4fb6774df8
misha <mentegy@gmail.com>
2015-05-05 14:03:18 +0300

    add vesions and some web

--- Web/src/main/webapp/resources/app/client/controllers/AccountController.js
+++ Web/src/main/webapp/resources/app/client/controllers/AccountController.js
@@ -2,5 +2,8 @@ angular
     .module('clientModule')
     .controller('AccountController', ['$scope', '$http', function ($scope, $http) {
         $scope.hello = "Hello Account";
+        $scope.click1 = function () {
+            $http.post("client", $scope.click1);
+        }
     }]);
 
