f2d528c79aa6c5a393877932a31cf8b5f78549be
Dmytro <gwini777@gmail.com>
2015-05-06 00:05:34 +0300

    Make pure Java-based configuration.

--- web/src/main/java/com/softserve/edu/config/WebAppInitializer.java
+++ web/src/main/java/com/softserve/edu/config/WebAppInitializer.java
@@ -1,31 +1,32 @@
 package com.softserve.edu.config;
 
-import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
 
 /**
- *
- * Replacement for most of the content of web.xml, sets up the root and the servlet context config.
- *
+ * Replacement for web.xml, sets up the root and the servlet context config.
  */
-public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+public class WebAppInitializer implements WebApplicationInitializer {
 
     @Override
-    protected Class<?>[] getRootConfigClasses() {
-        return new Class<?>[]{TransactionConfiguration.class, JPAConfiguration.class};
+    public void onStartup(ServletContext container) throws ServletException {
+        AnnotationConfigWebApplicationContext rootContext = new AnnotationConfigWebApplicationContext();
+        rootContext.register(TransactionConfiguration.class, JPAConfiguration.class);
+        container.addListener(new ContextLoaderListener(rootContext));
+
+        AnnotationConfigWebApplicationContext dispatcherContext =
+                new AnnotationConfigWebApplicationContext();
+        dispatcherContext.register(ServletContextConfig.class);
+        ServletRegistration.Dynamic dispatcher =
+                container.addServlet("dispatcher", new DispatcherServlet(dispatcherContext));
+        dispatcher.setLoadOnStartup(1);
+        dispatcher.addMapping("/");
     }
-
-    @Override
-    protected Class<?>[] getServletConfigClasses() {
-        return new Class<?>[] {ServletContextConfig.class};
-    }
-
-    @Override
-    protected String[] getServletMappings() {
-        return new String[]{"/"};
-    }
-
-
-
-
 }
 
