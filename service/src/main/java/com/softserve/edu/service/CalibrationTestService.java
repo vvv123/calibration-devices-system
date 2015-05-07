2af12016d14177917918225d69b6a63895c93c7d
Sander1992 <sander92@inbox.ru>
2015-05-07 16:58:06 +0300

    add resources for spring hateoas, necessary getters and setters, implemented service

---
+++ service/src/main/java/com/softserve/edu/service/CalibrationTestService.java
@@ -0,0 +1,36 @@
+package com.softserve.edu.service;
+
+
+import com.softserve.edu.entity.CalibrationTest;
+import com.softserve.edu.entity.CalibrationTestData;
+import com.softserve.edu.repository.CalibrationTestRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class CalibrationTestService {
+
+    @Autowired
+    private CalibrationTestRepository repository;
+
+    public CalibrationTest deleteTest(Long id){
+        CalibrationTest deletedCalibrationTest = repository.findOne(id);
+        repository.delete(id);
+        return deletedCalibrationTest;
+    }
+
+    public CalibrationTest findOne(Long id){
+        CalibrationTest calibrationTest = repository.findOne(id);
+        return calibrationTest;
+    }
+
+    public CalibrationTest updateCalibrationTest(Long id, CalibrationTest calibrationTest){
+        CalibrationTest updatedCalibrationTest = repository.findOne(id);
+        updatedCalibrationTest = repository.save(calibrationTest);
+        return updatedCalibrationTest;
+    }
+}
