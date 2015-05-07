fb8bda34e5b6da6b88cea5606365576dd5807ed3
Oles Onyshchak <oles1719@gmail.com>
2015-05-07 04:09:32 +0300

    add form and client validation(still in process)

--- web/src/main/webapp/resources/app/client/controllers/AccountController.js
+++ web/src/main/webapp/resources/app/client/controllers/AccountController.js
@@ -1,7 +1,7 @@
 angular
     .module('clientModule')
     .controller('AccountController', ['$scope', '$http', function ($scope, $http) {
-        $scope.hello = "Hello Account";
+        $scope.hello = "Hello Account A";
         $scope.click1 = function () {
             $http.post("client", $scope.click1);
         }
--- web/src/main/webapp/resources/app/client/controllers/AddApplicationsController.js
+++ web/src/main/webapp/resources/app/client/controllers/AddApplicationsController.js
@@ -1,6 +1,52 @@
 angular
     .module('clientModule')
-    .controller('AddApplicationsController', ['$scope', '$http', function ($scope, $http) {
-        $scope.hello = "Hello Add App";
+    .controller('AddApplicationsController', ['$scope', function($scope) {
+        $scope.formInfo = {};
+        $scope.saveData = function() {
+            $scope.nameRequired = '';
+            $scope.lastNameRequired = '';
+            $scope.middleNameRequired = '';
+            $scope.regionRequired = '';
+            $scope.localityRequired = '';
+            $scope.districtRequired = '';
+            $scope.streetRequired = '';
+            $scope.buildingRequired = '';
+            $scope.emailRequired = '';
+            $scope.phoneRequired = '';
+
+
+            if (!$scope.formInfo.name) {
+                $scope.nameRequired = 'name is required';
+            }
+
+            if (!$scope.formInfo.lastName) {
+                $scope.emailRequired = 'last name is required';
+            }
+
+            if (!$scope.formInfo.middleName) {
+                $scope.passwordRequired = 'middle name is required';
+            }
+            if (!$scope.formInfo.region) {
+                $scope.passwordRequired = 'region is required';
+            }
+            if (!$scope.formInfo.locality) {
+                $scope.passwordRequired = 'locality is required';
+            }
+            if (!$scope.formInfo.district) {
+                $scope.passwordRequired = 'district is required';
+            }
+            if (!$scope.formInfo.street) {
+                $scope.passwordRequired = 'street is required';
+            }
+            if (!$scope.formInfo.building) {
+                $scope.passwordRequired = 'building is required';
+            }
+            if (!$scope.formInfo.email) {
+                $scope.passwordRequired = 'email is required';
+            }
+            if (!$scope.formInfo.phone) {
+                $scope.passwordRequired = 'phone is required';
+            }
+        };
     }]);
 
