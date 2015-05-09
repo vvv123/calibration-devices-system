e660184453475f6b01657e76654fc8c9ae693b11
oleh.chernygevych <oleh.chernygevych@gmail.com>
2015-05-09 19:33:50 +0300

    change api, access to class now possible only using static function

--- service/src/main/java/com/softserve/edu/documentGenerator/generator/FormFiller.java
+++ service/src/main/java/com/softserve/edu/documentGenerator/generator/FormFiller.java
@@ -4,25 +4,34 @@ import com.softserve.edu.documentGenerator.utils.DocumentUtils;
 import org.apache.poi.hwpf.HWPFDocument;
 import org.apache.poi.poifs.filesystem.POIFSFileSystem;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 
 public class FormFiller {
+    private FormFiller() {
+    }
+
     /**
      * Replace token in a file and save changes
      */
-    public void replaceTokenAndSave(String token, String newText, String fileName, String newFileName) {
-        String filePath = new DocumentUtils().getFilePath(fileName);
+    public static File replaceTokenAndSave(String token, String newText, File file, String newFileName) {
+        //String filePath = new DocumentUtils().getFilePath(fileName);
         POIFSFileSystem documentFileStream = null;
 
         try {
-            documentFileStream = new POIFSFileSystem(new FileInputStream(filePath));
+            documentFileStream = new POIFSFileSystem(new FileInputStream(file));
             HWPFDocument doc = new HWPFDocument(documentFileStream);
             DocumentUtils.replaceText(doc, token, newText);
             DocumentUtils.saveMSWordDocument(newFileName, doc);
         } catch(IOException e){
             e.printStackTrace();
         }
+
+        return new File(newFileName);
     }
 
+    public static File getReadyTemplate(File template, int verificationID, String outputFileName) {
+        return replaceTokenAndSave("$USER", "Олег", template, outputFileName);
+    }
 }
