65872b370de64fab4212a44ce6bb0b226b8cd941
oleh.chernygevych <oleh.chernygevych@gmail.com>
2015-05-09 17:21:45 +0300

    add constants for standart paths

--- service/src/main/java/com/softserve/edu/documentGenerator/utils/StandardPaths.java
+++ service/src/main/java/com/softserve/edu/documentGenerator/utils/StandardPaths.java
@@ -1,16 +1,39 @@
 package com.softserve.edu.documentGenerator.utils;
 
 /**
- * Created by oleg on 5/9/15.
+ * Constants for standard paths
  */
 public enum StandardPaths {
-    DOCUMENTS_TEMPLATES("documentsTemplates"),
-    DOCUMENTS("documents"),
-    FONTS("documentsTemplates");
+    DOCUMENTS_TEMPLATES {
+        @Override
+        public String toString() {
+            return "documentsTemplates";
+        }
+    },
+    DOCUMENTS_GENERATED {
+        @Override
+        public String toString() {
+            return "documents";
+        }
+    },
+    FONTS {
+        @Override
+        public String toString() {
+            return "fonts";
+        }
+    };
 
-    String path;
-
-    StandardPaths(String path) {
-        this.path = path;
-    }
+//    private String path;
+//
+////    StandardPaths(String path) {
+////        this.path = path;
+////    }
+//
+//    public String getPath() {
+//        return path;
+//    }
+//
+//    public void setPath(String path) {
+//        this.path = path;
+//    }
 }
