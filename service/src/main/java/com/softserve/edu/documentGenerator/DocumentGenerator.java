67192106febd066ddd74aac3b4b1debbe35c6599
oleh.chernygevych <oleh.chernygevych@gmail.com>
2015-05-12 00:42:23 +0300

    static method now takes document instead of document type and verification id

--- service/src/main/java/com/softserve/edu/documentGenerator/DocumentGenerator.java
+++ service/src/main/java/com/softserve/edu/documentGenerator/DocumentGenerator.java
@@ -3,20 +3,21 @@ package com.softserve.edu.documentGenerator;
 import com.softserve.edu.documentGenerator.converter.Converter;
 import com.softserve.edu.documentGenerator.converter.Converters;
 import com.softserve.edu.documentGenerator.generator.FormFiller;
+import com.softserve.edu.documentGenerator.generator.documents.Document;
 import com.softserve.edu.documentGenerator.utils.DocumentFormat;
 import com.softserve.edu.documentGenerator.utils.PathBuilder;
 import com.softserve.edu.documentGenerator.utils.StandardPath;
-import com.softserve.edu.documentGenerator.utils.Template;
 
 import java.io.File;
 
 public class DocumentGenerator {
-    static public File generate(Template templateFile, int verificationID, DocumentFormat documentFormat) {
-        File template = templateFile.getFile();
-        String outputFileName = PathBuilder.build(StandardPath.DOCUMENTS_GENERATED, String.valueOf(verificationID),
+    static public File generate(Document document, DocumentFormat documentFormat) {
+        File template = document.getTemplate().getFile();
+        String outputFileName = PathBuilder.build(StandardPath.DOCUMENTS_GENERATED,
+                String.valueOf(document.getVerificationID()),
                 documentFormat);
 
-        File readyTemplate = FormFiller.getReadyTemplate(template, verificationID);
+        File readyTemplate = FormFiller.getReadyTemplate(template, document);
 
         Converter converter = Converters.get(documentFormat);
         File result = converter.getConvertedFile(readyTemplate, outputFileName);
