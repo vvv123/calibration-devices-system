46552f171297e7e7fd4fe423c3c72efff5abe0cb
oleh.chernygevych <oleh.chernygevych@gmail.com>
2015-05-11 21:34:52 +0300

    delete demoMain, create simple test for pdf generation

--- service/src/test/java/documentGenerationTests/GenerateFileTest.java
+++ service/src/test/java/documentGenerationTests/GenerateFileTest.java
@@ -1,21 +1,25 @@
-package com.softserve.edu.documentGenerator.core;
+package documentGenerationTests;
 
 import com.softserve.edu.documentGenerator.DocumentGenerator;
 import com.softserve.edu.documentGenerator.utils.DocumentFormat;
 import com.softserve.edu.documentGenerator.utils.Template;
+import com.softserve.edu.entity.Address;
+import com.softserve.edu.entity.ClientData;
+import com.softserve.edu.entity.Verification;
+import org.junit.Test;
 
 import java.io.File;
 
-/**
- * class for testing purposes, not for production
- */
-public class DemoMain {
+public class GenerateFileTest {
 
-    public static void main(String[] args) {
-        int verificationID = -1;
+    @Test
+    public void generatePdfTest() {
+        int verificationID = 1;
 
         File generatedDoc =
                 DocumentGenerator.generate(Template.DEVICE_CHECK, verificationID, DocumentFormat.PDF);
+
+        assert generatedDoc != null;
     }
 
 }
