e5b7d6f854d77de91141ea9917b29c0e5e9045a2
Oles Onyshchak <oles1719@gmail.com>
2015-05-12 18:27:38 +0300

    add applicationView to another file

--- web/src/main/webapp/resources/app/welcome/welcomeModule.js
+++ web/src/main/webapp/resources/app/welcome/welcomeModule.js
@@ -1,21 +1,5 @@
 angular.module('welcomeModule', ['spring-security-csrf-token-interceptor',
-    'duScroll', 'ngRoute', 'ui.bootstrap'])
-    .config(['$routeProvider',
-        function($routeProvider) {
-            //$locationProvider.html5Mode(true);
-            $routeProvider.
-                when('/', {
-                    controller: "AddApplicationsController",
-                    templateUrl: "/resources/app/welcome/views/AddApplicationsView.html"
-                }).
-                when('/check-app', {
-                    controller: "CheckApplicationController",
-                    templateUrl: "/resources/app/welcome/views/CheckApplication.html"
-                }).
-                otherwise({
-                    redirectTo: '/'
-                });
-        }]);
+    'duScroll', 'ui.bootstrap']);
 define([
     'controllers/GlobalController',
     'controllers/LoginController',
