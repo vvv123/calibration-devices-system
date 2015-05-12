deac5fa5736197fae01dc9adbb81c9badd3db2b2
Oles Onyshchak <oles1719@gmail.com>
2015-05-12 16:21:16 +0300

    change checkApplicationController

--- web/src/main/webapp/resources/app/welcome/controllers/CheckApplicationController.js
+++ web/src/main/webapp/resources/app/welcome/controllers/CheckApplicationController.js
@@ -1,14 +1,15 @@
 angular
     .module('welcomeModule')
-    .controller('CheckApplicationController',['$scope', '$http',  function($scope, $http) {
+    .controller('CheckApplicationController',[ '$scope', '$http',  function($scope, $http) {
+        $scope.myAlert = true;
         $scope.search = function() {
             var response = $http.post('/application/check/', $scope.codeInfo);
             response.success(function(data) {
-                $scope.status = JSON.stringify(data);
+                $scope.status =JSON.stringify(data.name);
+                $scope.myAlert = false;
             });
-            response.error(function(data) {
+            response.error(function(data, status, headers, config) {
                 console.dir(data);
             });
-
         };
     }]);
