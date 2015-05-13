b2173f7210242f39c858d59277c09b3c1daa65e8
Oles Onyshchak <oles1719@gmail.com>
2015-05-13 10:50:26 +0300

    change application controller name

--- web/src/main/java/com/softserve/edu/controller/ApplicationController.java
+++ web/src/main/java/com/softserve/edu/controller/ApplicationController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.*;
  *
  */
 @RestController
-public class /**/ApplicationController {
+public class ApplicationController {
 
     @Autowired
     private ClientService clientService;
