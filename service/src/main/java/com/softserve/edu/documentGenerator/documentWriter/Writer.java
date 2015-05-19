88739f9ef4974b0b8ffc9db9e2353ede312d58a7
oleh.chernygevych <oleh.chernygevych@gmail.com>
2015-05-19 16:51:26 +0300

    move resources to web layer

--- service/src/main/java/com/softserve/edu/documentGenerator/documentWriter/Writer.java
+++ service/src/main/java/com/softserve/edu/documentGenerator/documentWriter/Writer.java
@@ -1,8 +1,5 @@
 package com.softserve.edu.documentGenerator.documentWriter;
 
-/**
- * Created by oleg on 17.05.15.
- */
 public interface Writer {
     String replaceTokens(String text);
 }
--- web/src/main/java/com/softserve/edu/controller/ApplicationController.java
+++ web/src/main/java/com/softserve/edu/controller/ApplicationController.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.*;
  */
 @RestController
 public class ApplicationController {
-
     @Autowired
     private ClientService clientService;
 
