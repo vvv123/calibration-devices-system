c199eebbc977fd66a7c3775d73191ada6f40a68f
misha <mentegy@gmail.com>
2015-05-26 19:36:41 +0300

    code style

--- web/src/main/java/com/softserve/edu/controller/provider/ProviderController.java
+++ web/src/main/java/com/softserve/edu/controller/provider/ProviderController.java
@@ -23,6 +23,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping(value = "/provider/verifications/")
 public class ProviderController {
 
     @Autowired
@@ -36,61 +37,83 @@ public class ProviderController {
 
     private final Logger logger = Logger.getLogger(ProviderController.class);
 
-    @RequestMapping(value = "/provider/verifications/archive/{pageNumber}/{itemsPerPage}", method = RequestMethod.GET)
+    @RequestMapping(value = "archive/{pageNumber}/{itemsPerPage}", method = RequestMethod.GET)
     public PageDTO<VerificationPageDTO> getPageOfAllVerificationsByProviderId(
             @PathVariable Integer pageNumber,
             @PathVariable Integer itemsPerPage,
             @AuthenticationPrincipal SecurityUserDetailsService.CustomUserDetails employeeUser) {
 
-        Page<VerificationPageDTO> page = VerificationPageDTOTransformer.toDTO(verificationService
-                .findPageOfAllVerificationsByProviderId(employeeUser.getOrganizationId(), pageNumber, itemsPerPage));
+        Page<VerificationPageDTO> page = VerificationPageDTOTransformer
+                .toDTO(verificationService
+                        .findPageOfAllVerificationsByProviderId(
+                                employeeUser.getOrganizationId(),
+                                pageNumber,
+                                itemsPerPage
+                        ));
+
         return new PageDTO<>(page.getTotalElements(), page.getContent());
     }
 
-    @RequestMapping(value = "/provider/verifications/new/{pageNumber}/{itemsPerPage}", method = RequestMethod.GET)
+    @RequestMapping(value = "new/{pageNumber}/{itemsPerPage}", method = RequestMethod.GET)
     public PageDTO<VerificationPageDTO> getPageOfAllSentVerificationsByProviderId(
             @PathVariable Integer pageNumber,
             @PathVariable Integer itemsPerPage,
             @AuthenticationPrincipal SecurityUserDetailsService.CustomUserDetails employeeUser) {
 
-        Page<VerificationPageDTO> page = VerificationPageDTOTransformer.toDTO(verificationService
-                .findPageOfSentVerificationsByProviderId(employeeUser.getOrganizationId(), pageNumber, itemsPerPage));
+        Page<VerificationPageDTO> page = VerificationPageDTOTransformer
+                .toDTO(verificationService
+                        .findPageOfSentVerificationsByProviderId(
+                                employeeUser.getOrganizationId(),
+                                pageNumber,
+                                itemsPerPage));
+
         return new PageDTO<>(page.getTotalElements(), page.getContent());
     }
 
-    @RequestMapping(value = "/provider/verifications/new/calibrators", method = RequestMethod.GET)
+    @RequestMapping(value = "new/calibrators", method = RequestMethod.GET)
     public List<Calibrator> updateVerification(
             @AuthenticationPrincipal SecurityUserDetailsService.CustomUserDetails employeeUser) {
 
         return calibratorService.findByDistrict(
-                providerService.findById(employeeUser.getOrganizationId()).getAddress().getDistrict());
+                providerService
+                        .findById(employeeUser.getOrganizationId())
+                        .getAddress()
+                        .getDistrict()
+        );
     }
 
 
-    @RequestMapping(value = "/provider/verifications/new/update", method = RequestMethod.PUT)
+    @RequestMapping(value = "new/update", method = RequestMethod.PUT)
     public void updateVerification(@RequestBody VerificationsAndCalibrationIdsDTO verificationsAndCalibrationIdsDTO) {
 
         for (String verificationId : verificationsAndCalibrationIdsDTO.getVerificationIds()) {
-            verificationService.updateVerification(verificationId, verificationsAndCalibrationIdsDTO.getCalibrator());
+            verificationService
+                    .updateVerification(
+                            verificationId,
+                            verificationsAndCalibrationIdsDTO.getCalibrator());
         }
     }
 
-    @RequestMapping(value = "/provider/verifications/new/{verificationId}", method = RequestMethod.GET)
+    @RequestMapping(value = "new/{verificationId}", method = RequestMethod.GET)
     public ClientStageVerificationDTO getNewVerificationDetailsById(
             @PathVariable String verificationId,
             @AuthenticationPrincipal SecurityUserDetailsService.CustomUserDetails employeeUser) {
 
-        Verification verification = verificationService.findByIdAndProviderId(verificationId, employeeUser.getOrganizationId());
+        Verification verification = verificationService
+                .findByIdAndProviderId(verificationId, employeeUser.getOrganizationId());
+
         ClientData clientData = verification.getClientData();
+
         return new ClientStageVerificationDTO(clientData, clientData.getClientAddress(), null);
     }
 
-    @RequestMapping(value = "/provider/verifications/archive/{verificationId}", method = RequestMethod.GET)
+    @RequestMapping(value = "archive/{verificationId}", method = RequestMethod.GET)
     public VerificationDTO getArchivalVerificationDetailsById(
             @PathVariable String verificationId,
             @AuthenticationPrincipal SecurityUserDetailsService.CustomUserDetails employeeUser) {
 
-        Verification verification = verificationService.findByIdAndProviderId(verificationId, employeeUser.getOrganizationId());
+        Verification verification = verificationService
+                .findByIdAndProviderId(verificationId, employeeUser.getOrganizationId());
 
         return new VerificationDTO(verification.getClientData(), verification.getId(), verification.getInitialDate(),
                 verification.getExpirationDate(), verification.getStatus(), verification.getCalibrator(),
